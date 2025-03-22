8594e2a9c8dae22b4377cd49e2737257b19300bc
Sergio <ivanov.sergey.vad@mail.ru>
2025-03-22 17:41:43 +0300

    123

--- server/app.js
+++ server/app.js
@@ -35,55 +35,53 @@ mongoose.set('strictQuery', false);
 async function start() {
     try {
         // if (config.get('localhost')) {
-            // await mongoose.connect(
-            //     'mongodb://uskuez0i3iuni7whxnho:B1bwvGmm03CiEnVGiwBe@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bs8zzjhjnhiz2va?replicaSet=rs0',
-            // );
-            // httpServer.listen(8080, () =>
-            //     console.log(
-            //         chalk.green(`Server HTTP has been started on port 8080...`),
-            //     ),
-            // );
+        // await mongoose.connect(
+        //     'mongodb://uskuez0i3iuni7whxnho:B1bwvGmm03CiEnVGiwBe@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bs8zzjhjnhiz2va?replicaSet=rs0',
+        // );
+        // httpServer.listen(8080, () =>
+        //     console.log(
+        //         chalk.green(`Server HTTP has been started on port 8080...`),
+        //     ),
+        // );
         // } else {
-            await mongoose.connect('mongodb://admin:12345@82.148.31.29:27017/');
-            var httpsServer = https.createServer(
-                {
-                    key: fs.readFileSync(
-                        path.resolve(
-                            __dirname,
-                            '..',
-                            '..',
-                            '..',
-                            'etc',
-                            'letsencrypt',
-                            'live',
-                            'mebeluxury.ru',
-                            'privkey.pem',
-                        ),
-                    ), // путь к ключу
-                    cert: fs.readFileSync(
-                        path.resolve(
-                            __dirname,
-                            '..',
-                            '..',
-                            '..',
-                            'etc',
-                            'letsencrypt',
-                            'live',
-                            'mebeluxury.ru',
-                            'fullchain.pem',
-                        ),
-                    ), // путь к сертификату
-                },
-                app,
-            );
-
-            httpsServer.listen(8443, () =>
-                console.log(
-                    chalk.green(
-                        `Server HTTPS has been started on port 8443...`,
+        await mongoose.connect('mongodb://admina:pwd@82.148.31.29:27017/');
+        var httpsServer = https.createServer(
+            {
+                key: fs.readFileSync(
+                    path.resolve(
+                        __dirname,
+                        '..',
+                        '..',
+                        '..',
+                        'etc',
+                        'letsencrypt',
+                        'live',
+                        'mebeluxury.ru',
+                        'privkey.pem',
                     ),
-                ),
-            );
+                ), // путь к ключу
+                cert: fs.readFileSync(
+                    path.resolve(
+                        __dirname,
+                        '..',
+                        '..',
+                        '..',
+                        'etc',
+                        'letsencrypt',
+                        'live',
+                        'mebeluxury.ru',
+                        'fullchain.pem',
+                    ),
+                ), // путь к сертификату
+            },
+            app,
+        );
+
+        httpsServer.listen(8443, () =>
+            console.log(
+                chalk.green(`Server HTTPS has been started on port 8443...`),
+            ),
+        );
         // }
     } catch (error) {
         console.log(chalk.red(error.message));
